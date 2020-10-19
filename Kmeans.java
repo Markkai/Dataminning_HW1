f22ef9bf7dc9538d9405efa7734c52ca87a46ce7
Mark Deng <52767117+Markkai@users.noreply.github.com>
2020-10-19 22:06:52 +0800

    Update Kmeans.java

--- Kmeans.java
+++ Kmeans.java
@@ -12,50 +12,61 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Kmeans {
 
- public static class KM_Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 
+ public static class KM_Map extends Mapper<LongWritable, Text, Text, IntWritable> {
     private List<String> centerID = new ArrayList<>();
     private List<Integer> centerValue = new ArrayList<>();
 
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        StringTokenizer tokenizer = new StringTokenizer(line);
- 
 
-        // 群ID
-        centerID.add("K1");
-        centerID.add("K2");
-        centerID.add("K3");
-        centerID.add("K4");
-        // 隨機4中心
+    public void setup(Contex contex){
+
+        // Random 給4中心點
         centerValue.add(20);
         centerValue.add(30);
         centerValue.add(50);
         centerValue.add(70);
+        // 群中心ID
+        centerID.add("K1");
+        centerID.add("K2");
+        centerID.add("K3");
+        centerID.add("K4");
+    }
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
 
         while(tokenizer.hasMoreTokens()){
-            String column1 = tokenizer.nextToken(); // Date
-            String column2 = tokenizer.nextToken(); // Dali
-            String column3 = tokenizer.nextToken(); //PM2.5
+            String column1 = tokenizer.nextToken(); //Date
+            String column2 = tokenizer.nextToken(); //Area
+            String column3 = tokenizer.nextToken(); //PM25
 
             int new_value = Integer.parseInt(column3);
             List<Double> list_distance = new ArrayList<>();
-
-            // 第一個值和 K1,2,3,4 算距離找最小
-            for(int i = 0;i< centerValue.size();i++){ 
+            
+            // 算距離
+            for(int i =0; i< centerValue.size(); i++){ 
                 double distance = Math.abs(new_value - centerValue.get(i));
                 list_distance.add(distance);
             }
             // 找出最近中心點並分類
-            for(int i= 0;i< centerValue.size();i++){
-                if(Collections.min(list_distance) == Math.abs(new_value - centerValue.get(id))){
+            for(int i= 0; i< centerValue.size(); i++){
+                if(Collections.min(list_distance) == Math.abs(new_value - centerValue.get(i))){
+                    
                     context.write(new Text(centerID.get(i)), new IntWritable(new_value));
                 }
             }
         }
     }
- }
+    /*
+    private int loadNew_km(Path path) throws IOException{
+        Configuration config = new Configuration();
+        Filesystem hdfs = Filesystem.get(config);
+        FileStatus[] files = hdfs.listStatus(path);
+    }*/
 
+
+ }
  public static class KM_Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
     private int sum = 0;
     private int count = 0;
@@ -68,50 +79,38 @@ public class Kmeans {
         context.write(key, new IntWritable(sum/count));
     }
  }
- 
-}
+
+
 
 public static void main(String[] args) throws Exception {
     
-    Configuration conf = new Configuration();                                                                                                                                
-    Job job = new Job(conf, "Kmeans");
 
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    for(int i= 0; i<1; i++){
 
-    job.setMapperClass(KM_Map.class);
-    job.setReducerClass(KM_Reduce.class);
-    job.setJarByClass(Kmeans.class);
+        Configuration conf = new Configuration();                                                                                                                                
+        Job job = new Job(conf, "Kmeans");
 
-    job.setInputFormatClass(TextInputFormat.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(KM_Map.class);
+        job.setReducerClass(KM_Reduce.class);
+        job.setJarByClass(Kmeans.class);
 
-    job.waitForCompletion(true);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+
+    }
 
 
-    /*
-    Configuration conf2 = new Configuration();
 
-    Job job2 = new Job(conf2, "Sort_count");
-    job2.setOutputKeyClass(IntWritable.class);
-    job2.setOutputValueClass(Text.class);
-
-    job.setMapperClass(SortMap.class);
-    job.setReducerClass(SortReduce.class);
-    job.setJarByClass(WordCount.class);
-
-    job.setInputFormatClass(TextInputFormat.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
-
-    FileInputFormat.addInputPath(job, new Path(args[1]));
-    FileOutputFormat.setOutputPath(job, new Path(args[2]));
-    job2.waitForCompletion(true);
-*/
+  }
 }
 
 
 
- 
